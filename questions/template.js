846a7124b5bf43c23aca089a79447ba24fa32cb8
penguin-k <32879754+penguin-k@users.noreply.github.com>
2018-05-18 21:13:51 +0100

    New questions

---
+++ questions/template.js
@@ -0,0 +1,155 @@
+var questionset = {
+  "stage1": [
+    {"Q": "New Dheli is located in which country?",
+      "Y": "India",
+      "N1": "Pakistan",
+      "N2": "China",
+      "N3": "Japan"
+    },
+    {"Q": "What is the name of the virtual assistant on iPhones?",
+      "Y": "Siri",
+      "N1": "Sarah",
+      "N2": "Shelly",
+      "N3": "Sophie"
+    },
+    {"Q": "Which of these words appears in the NATO phonetic alphabet?",
+      "Y": "Golf",
+      "N1": "Basketball",
+      "N2": "Netball",
+      "N3": "Hockey"
+    },
+    {"Q": "Which number is equal to 5 times the sum of its digits?",
+      "Y": "45",
+      "N1": "20",
+      "N2": "39",
+      "N3": "67"
+    },
+    {"Q": "Which of these is a German city?",
+      "Y": "Hamburg",
+      "N1": "Himburg",
+      "N2": "Homburg",
+      "N3": "Humburg"
+    }
+  ],
+  "stage2": [
+    {"Q": "What is the name of the dog in 'The Famous Five'?",
+      "Y": "Timmy",
+      "N1": "Tommy",
+      "N2": "Tammy",
+      "N3": "Tummy"
+    },
+    {"Q": "In the 'Harry Potter' books, which house was Hermione in?",
+      "Y": "Gryffindor",
+      "N1": "Hufflepuff",
+      "N2": "Ravenclaw",
+      "N3": "Slytherin"
+    },
+    {"Q": "What links the words 'kayak' and 'racecar'?",
+      "Y": "Palindromes",
+      "N1": "Oxymorons",
+      "N2": "Onopatopoeia",
+      "N3": "Anagrams"
+    },
+    {"Q": "Until 2017, Charles Darwin appeared on an English banknote. What denomination was it?",
+      "Y": "£10",
+      "N1": "£5",
+      "N2": "£20",
+      "N3": "£50"
+    },
+    {"Q": "What were the Seven Dwarfs mining for?",
+      "Y": "Diamonds",
+      "N1": "Gold",
+      "N2": "Silver",
+      "N3": "Quartz"
+    },
+    {"Q": "In the nursery rhyme, what is Jack's head treated with after he falls down the hill?",
+      "Y": "Vinegar and brown paper",
+      "N1": "Bandages",
+      "N2": "Margarine",
+      "N3": "Cream"
+    },
+    {"Q": "Someone who suffurs from photoic sneeze reflex sneezes in response to what?",
+      "Y": "Sunlight",
+      "N1": "Loud noises",
+      "N2": "Salty foods",
+      "N3": "Strange smells"
+    }
+  ],
+  "stage3": [
+    {"Q": "Which of these is in group 0 of the Periodic Table?",
+      "Y": "Neon",
+      "N1": "Fluorine",
+      "N2": "Hydrogen",
+      "N3": "Oxygen"
+    },
+    {"Q": "The Nordic Council has no members from which of these countries?",
+      "Y": "Estonia",
+      "N1": "Greenland",
+      "N2": "Iceland",
+      "N3": "Finland"
+    },
+    {
+      "Q": "When did humans last land on the moon?",
+      "Y": "1972",
+      "N1": "1971",
+      "N2": "1973",
+      "N3": "1974"
+    },
+    {
+      "Q": "In relation to its epicentre, where is an earthquake's hypocentre?",
+      "Y": "Directly below",
+      "N1": "2 miles across land",
+      "N2": "The nearest ocean",
+      "N3": "The nearest village"
+    },
+    {
+      "Q": "Who invented a test which could define a computer as being 'sentient'?",
+      "Y": "Alan Turing",
+      "N1": "Niels Bohr",
+      "N2": "Steve Jobs",
+      "N3": "Bill Gates"
+    }
+  ],
+  "stage4": [
+    {"Q": "Svalbard, an island in the Arctic Ocean, is officially part of which country?",
+      "Y": "Svalbard and Jan Mayen",
+      "N1": "Svalbard and Norway",
+      "N2": "Svalbard and Iceland",
+      "N3": "Svalbard"
+    },
+    {"Q": "Which Asian country has a population of around 75 million?",
+      "Y": "Iran",
+      "N1": "Iraq",
+      "N2": "Vietnam",
+      "N3": "Burma"
+    },
+    {
+      "Q": "How many British town have a name ending with 'mouth'?",
+      "Y": "29",
+      "N1": "9",
+      "N2": "19",
+      "N3": "39"
+    },
+    {
+      "Q": "The 'Simplon Pass' connects Switzerland and which other country?",
+      "Y": "Italy",
+      "N1": "Austria",
+      "N2": "France",
+      "N3": "Germany"
+    },
+    {
+      "Q": "What is the minimum number of strokes required to score 147 in a game of snooker?",
+      "Y": "36",
+      "N1": "22",
+      "N2": "17",
+      "N3": "47"
+    },
+    {
+      "Q": "Which string of digits appears in the first 20 digits of pi?",
+      "Y": "6535",
+      "N1": "9262",
+      "N2": "4158",
+      "N3": "3572"
+    }
+  ]
+};
