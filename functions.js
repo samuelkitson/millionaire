178f60ee4e577099b24ddef2bbd6cabe99562edb
penguin-k <32879754+penguin-k@users.noreply.github.com>
2018-05-21 17:15:32 +0100

    Update

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function checkMobile() {
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     return "true";
   } else {
     return "false";
