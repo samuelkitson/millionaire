ba27b82fdf2fd04fabd9469ca7e9c7f0dacf393b
penguin-k <32879754+penguin-k@users.noreply.github.com>
2018-06-23 12:32:29 +0100

    Updated questions

--- questions/records.js
+++ questions/records.js
@@ -12,6 +12,13 @@ var questionset = {
       "N1": "Tallest Railway Station",
       "N2": "Highest Railway Station",
       "N3": "Busiest Railway Station"
+    },
+    {
+      "Q": "What is the most common language in the world?",
+      "Y": "Chinese",
+      "N1": "English",
+      "N2": "Japanese",
+      "N3": "Russian"
     }
   ],
   "stage2": [
@@ -20,6 +27,27 @@ var questionset = {
       "N1": "12",
       "N2": "8",
       "N3": "5"
+    },
+    {
+      "Q": "Approximately how big is the largest collection of Scooby Doo memorabilia?",
+      "Y": "1,100",
+      "N1": "110",
+      "N2": "11,000",
+      "N3": "110,000"
+    },
+    {
+      "Q": "Sir David Attenborough holds the record for the longest TV presenter career. How long is it (so far)?",
+      "Y": "64 years",
+      "N1": "54 years",
+      "N2": "44 years",
+      "N3": "34 years"
+    },
+    {
+      "Q": "Which nation has won the largest number of Commonwealth Games medals?",
+      "Y": "Australia",
+      "N1": "New Zealand",
+      "N2": "Canada",
+      "N3": "India"
     }
   ],
   "stage3": [
@@ -35,6 +63,20 @@ var questionset = {
       "N1": "Altostratus",
       "N2": "Altocumulus",
       "N3": "Stratus"
+    },
+    {
+      "Q": "Where is the highest railway line in the world?",
+      "Y": "China",
+      "N1": "Japan",
+      "N2": "Germany",
+      "N3": "USA"
+    },
+    {
+      "Q": "When were emojis first invented?",
+      "Y": "1999",
+      "N1": "2009",
+      "N2": "2004",
+      "N3": "1994"
     }
   ],
   "stage4": [
@@ -43,6 +85,13 @@ var questionset = {
       "N1": "248m",
       "N2": "48m",
       "N3": "8m"
+    },
+    {
+      "Q": "When was the oldest surviving ariel photograph taken?",
+      "Y": "1860",
+      "N1": "1960",
+      "N2": "1900",
+      "N3": "1880"
     }
   ]
 };
