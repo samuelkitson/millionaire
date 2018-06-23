f86f94b7aaca1e5e8c72d9ae43657fac1970158b
penguin-k <32879754+penguin-k@users.noreply.github.com>
2018-06-23 12:08:13 +0100

    New question set!

---
+++ questions/records.js
@@ -0,0 +1,48 @@
+var questionset = {
+  "stage1": [
+    {"Q": "What is the fastest animal on Earth?",
+      "Y": "Cheetah",
+      "N1": "Lion",
+      "N2": "Jaguar",
+      "N3": "Leopard"
+    },
+    {
+      "Q": "Grand Central Terminal in New York is the...",
+      "Y": "Largest Railway Station",
+      "N1": "Tallest Railway Station",
+      "N2": "Highest Railway Station",
+      "N3": "Busiest Railway Station"
+    }
+  ],
+  "stage2": [
+    {"Q": "As of 2014, how old was the oldest pig (ever)?",
+      "Y": "22",
+      "N1": "12",
+      "N2": "8",
+      "N3": "5"
+    }
+  ],
+  "stage3": [
+    {"Q": "On average, how fast is a 'mako' shark, the fastest type of shark?",
+      "Y": "60mph",
+      "N1": "40mph",
+      "N2": "80mph",
+      "N3": "20mph"
+    },
+    {
+      "Q": "Which of these is one of the highest types of clouds?",
+      "Y": "Cirrocumulus",
+      "N1": "Altostratus",
+      "N2": "Altocumulus",
+      "N3": "Stratus"
+    }
+  ],
+  "stage4": [
+    {"Q": "How tall is the smallest officially recognised mountain in the world?",
+      "Y": "148m",
+      "N1": "248m",
+      "N2": "48m",
+      "N3": "8m"
+    }
+  ]
+};
